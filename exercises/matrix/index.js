84840b08fde6baf8d495d816113512f7c5d8c755
joey <doode1524@gmail.com>
2021-07-12 19:11:32 -0400

    fib iterative solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -23,42 +23,42 @@ function matrix(n) {
   }
 
   let counter = 1;
-  let startColumn = 0;
-  let endColumn = n - 1;
   let startRow = 0;
+  let startColumn = 0;
   let endRow = n - 1;
+  let endColumn = n - 1;
 
   while (startColumn <= endColumn && startRow <= endRow) {
     // Top Row
-    for (let i = startColumn; i <= endColumn; i++){
-        results[startRow][i] = counter
-        counter++
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
     }
-    startRow++
+    startRow++;
 
     // Right Column
-    for (let i = startRow; i <= endRow; i++){
-        results[i][endColumn] = counter
-        counter++
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
     }
-    endColumn--
+    endColumn--;
 
     // Bottom Row
-    for (let i = endColumn; i >= startColumn; i--){
-        results[endRow][i] = counter;
-        counter++
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
     }
-    endRow--
+    endRow--;
 
     // Start Column
-    for (let i = endRow; i >= startRow; i--){
-        results[i][startColumn] = counter;
-        counter++
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
     }
-    startColumn++
+    startColumn++;
   }
 
-  return results
+  return results;
 }
 
 module.exports = matrix;
