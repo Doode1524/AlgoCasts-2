7d4c6e045029bcaea0f76a618a83096a41bf43f6
joey <doode1524@gmail.com>
2021-07-17 20:02:00 -0400

    getLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,34 +3,42 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next = null) {
-        this.data = data
-        this.next = next
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    node.next = this.head;
+    this.head = node;
+    // this.head = new Node(data, this.head
+  }
+  size() {
+    let pointer = this.head;
+    let count = 0;
+    while (pointer) {
+      pointer = pointer.next;
+      count++;
     }
-    insertFirst(data) {
-        const node = new Node(data, this.head)
-        node.next = this.head
-        this.head = node
-        // this.head = new Node(data, this.head
+    return count;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    let pointer = this.head;
+    while (pointer.next) {
+      pointer = pointer.next;
     }
-    size() {
-        let pointer = this.head
-        let count = 0
-        while (pointer) {
-            pointer = pointer.next
-            count ++
-        }
-        return count
-    }
-    getFirst() {
-        return this.head
-    }
+
+    return pointer;
+  }
 }
 
 module.exports = { Node, LinkedList };
