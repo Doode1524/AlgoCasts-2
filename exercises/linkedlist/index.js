7e8d093f4ba3eb8382d5573ad060ca3935991295
joey <doode1524@gmail.com>
2021-07-17 23:59:37 -0400

    insertAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -96,15 +96,27 @@ class LinkedList {
       return;
     }
     if (index === 0) {
-        this.head = this.head.next
-        return
+      this.head = this.head.next;
+      return;
     }
-    const previous = this.getAt(index - 1)
+    const previous = this.getAt(index - 1);
     if (!previous || !previous.next) {
-        return
+      return;
     }
-    previous.next = previous.next.next
-
+    previous.next = previous.next.next;
+  }
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const previous = this.getAt(index - 1) || this.getLast();
+    const node = new Node(data, previous.next);
+    previous.next = node;
   }
 }
 
