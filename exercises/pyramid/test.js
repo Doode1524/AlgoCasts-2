c28c17c142e03e25b6fd018400d79ee9581f85e8
joey <doode1524@gmail.com>
2021-11-22 21:21:41 -0500

    fix test

--- exercises/pyramid/test.js
+++ exercises/pyramid/test.js
@@ -43,6 +43,6 @@ test('prints a pryamid for n = 5', () => {
   expect(console.log.mock.calls[1][0]).toEqual('   ###   ');
   expect(console.log.mock.calls[2][0]).toEqual('  #####  ');
   expect(console.log.mock.calls[3][0]).toEqual(' ####### ');
-  expect(console.log.mock.calls[3][0]).toEqual('#########');
-  expect(console.log.mock.calls.length).toEqual(4);
+  expect(console.log.mock.calls[4][0]).toEqual('#########');
+  expect(console.log.mock.calls.length).toEqual(5);
 });
